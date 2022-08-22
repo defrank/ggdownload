dea5cb90457d6f5a57b8b97ef125d0502a016c49
Derek M Frank <derek@frank.sh>
2022-08-22 13:17:25 -0700

    chore(courses-pipeline): Create and enable basic pipeline for courses

--- grapplersguide/pipelines.py
+++ grapplersguide/pipelines.py
@@ -6,8 +6,21 @@
 
 # useful for handling different item types with a single interface
 # from itemadapter import ItemAdapter
+import functools as fn
+import logging
 
 
-class GrapplersGuidePipeline:
+class CoursePipeline:
+    def open_spider(self, spider):
+        self.logger.debug("Spider opened: %s", spider.name)
+
     def process_item(self, item, spider):
+        self.logger.debug("Pipeline processed %s item: %s", spider.name, item)
         return item
+
+    def close_spider(self, spider):
+        self.logger.debug("Spider closed: %s", spider.name)
+
+    @fn.cached_property
+    def logger(self):
+        return logging.getLogger(f"{__name__}.{self.__class__.__name__}")
