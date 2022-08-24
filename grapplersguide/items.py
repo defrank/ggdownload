d61aa7ab32821568464c94794471beb0c43fcd85
Derek M Frank <derek@frank.sh>
2022-08-23 20:23:27 -0700

    refactor(items,spiders): Move items to their own module

--- grapplersguide/items.py
+++ grapplersguide/items.py
@@ -2,11 +2,48 @@
 #
 # See documentation in:
 # https://docs.scrapy.org/en/latest/topics/items.html
+import dataclasses as dc
+from typing import FrozenSet, List, Optional, Tuple
 
-import scrapy
+
+@dc.dataclass(frozen=True)
+class Expert:
+    name: str
 
 
-class CourseItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+@dc.dataclass(frozen=True)
+class Course:
+    title: str
+    expert: Expert
+
+
+@dc.dataclass(frozen=True)
+class Section:
+    position: int
+    title: str
+    course: Course
+
+
+@dc.dataclass(frozen=True)
+class Lesson:
+    position: int
+    title: str
+    url: str
+    section: Section
+    breadcrumbs: Optional[Tuple[str, ...]] = None
+    tags: Optional[FrozenSet[str]] = None
+
+
+@dc.dataclass(frozen=True)
+class Video:
+    file_name: str
+    public_name: str
+    base_file_name: str
+    extension: str
+    download_name: str
+    size: str
+    height: int
+    width: int
+    video_file_id: str
+    download_url: List[str]
+    lesson: Lesson
--- grapplersguide/spiders.py
+++ grapplersguide/spiders.py
@@ -1,56 +1,15 @@
 import dataclasses as dc
 import operator as op
+import re
 import urllib.parse
-from typing import FrozenSet, Optional, Tuple
 
 import scrapy
 
-
-@dc.dataclass(frozen=True)
-class Expert:
-    name: str
+from . import items
 
 
-@dc.dataclass(frozen=True)
-class Course:
-    title: str
-    expert: Expert
-
-
-@dc.dataclass(frozen=True)
-class Section:
-    position: int
-    title: str
-    course: Course
-
-
-@dc.dataclass(frozen=True)
-class Lesson:
-    position: int
-    title: str
-    url: str
-    section: Section
-    breadcrumbs: Optional[Tuple[str, ...]] = None
-    tags: Optional[FrozenSet[str]] = None
-
-
-@dc.dataclass(frozen=True)
-class Download:
-    file_name: str
-    public_name: str
-    base_file_name: str
-    extension: str
-    download_name: str
-    size: str
-    height: int
-    width: int
-    video_file_id: str
-    download_url: str
-    lesson: Lesson
-
-
-class CoursesSpider(scrapy.Spider):
-    name = "courses"
+class ExpertCoursesSpider(scrapy.Spider):
+    name = "expert-courses"
     allowed_domains = [
         "grapplersguide.com",
         "thestrikersguide.com",
@@ -61,15 +20,24 @@ class CoursesSpider(scrapy.Spider):
         "https://grapplersguide.com/second-portal/login",
     ]
 
-    def __init__(self, username: str, password: str):
+    def __init__(
+        self,
+        username: str,
+        password: str,
+        expert_regex: str = r".+",
+        course_regex: str = r".+",
+    ):
         self._username = username
         self._password = password
+        self._expert_regex = re.compile(expert_regex)
+        self._course_regex = re.compile(course_regex)
         super().__init__()
 
     def start_requests(self):
         self.logger.debug("Starting requests...")
         for url in self.login_urls:
             yield scrapy.Request(url=url, callback=self.parse_login)
+            return
 
     def parse_login(self, response):
         self.logger.debug("Logging in...")
@@ -89,29 +57,31 @@ class CoursesSpider(scrapy.Spider):
             yield scrapy.Request(
                 url=response.urljoin(courses_path),
                 callback=self.parse_courses,
-                cb_kwargs={"expert": Expert(name=expert)},
+                cb_kwargs={"expert": items.Expert(name=expert)},
             )
+            return
 
-    def parse_courses(self, response, expert: Expert):
+    def parse_courses(self, response, expert: items.Expert):
         self.logger.debug("Parsing courses for expert, %s", expert)
         course_links = response.css("div.node-main div.node-title > a")
         for link in course_links:
             title = link.xpath("text()").get()
             course_path = link.attrib["href"]
-            course = Course(title=title, expert=expert)
+            course = items.Course(title=title, expert=expert)
             yield scrapy.Request(
                 url=response.urljoin(course_path),
                 callback=self.parse_course,
                 cb_kwargs={"course": course},
             )
+            return
 
-    def parse_course(self, response, course: Course):
+    def parse_course(self, response, course: items.Course):
         self.logger.debug("Parsing course: %s", course)
         lesson_sections = response.css("div.block-container")
         for section_index, section in enumerate(lesson_sections, 1):
             section_title = section.css("h2.block-header > a::text").get()
             lesson_links = section.css("h3.node-title > a")
-            section = Section(
+            section = items.Section(
                 position=section_index,
                 title=section_title,
                 course=course,
@@ -119,7 +89,7 @@ class CoursesSpider(scrapy.Spider):
             for link_index, link in enumerate(lesson_links, 1):
                 lesson_title = link.xpath("text()").get()
                 lesson_path = link.attrib["href"]
-                lesson = Lesson(
+                lesson = items.Lesson(
                     position=link_index,
                     title=lesson_title,
                     url=response.urljoin(link.attrib["href"]),
@@ -130,8 +100,9 @@ class CoursesSpider(scrapy.Spider):
                     callback=self.parse_lesson,
                     cb_kwargs={"lesson": lesson},
                 )
+                return
 
-    def parse_lesson(self, response, lesson: Lesson):
+    def parse_lesson(self, response, lesson: items.Lesson):
         self.logger.debug("Parsing lesson: %s", lesson)
 
         breadcrumb_links = response.xpath(
@@ -163,7 +134,7 @@ class CoursesSpider(scrapy.Spider):
             },
         )
 
-    def parse_download_page(self, response, lesson: Lesson):
+    def parse_download_page(self, response, lesson: items.Lesson):
         assert lesson.breadcrumbs is not None, "breadcrumbs must be a tuple"
         assert lesson.tags is not None, "tags must be a frozenset"
         self.logger.debug("Parsing download page: %s", lesson)
@@ -180,17 +151,17 @@ class CoursesSpider(scrapy.Spider):
             cb_kwargs={"lesson": lesson},
         )
 
-    def parse_download_data(self, response, lesson: Lesson):
+    def parse_download_data(self, response, lesson: items.Lesson):
         self.logger.debug("Parsing download data: %s", lesson)
         data = response.json()
         files = data["download_config"]["files"]
         highest_quality_file = max(files, key=op.itemgetter("height"))
-        download_fields = {field.name for field in dc.fields(Download)}
-        yield Download(
+        video_fields = {field.name for field in dc.fields(items.Video)}
+        yield items.Video(
             lesson=lesson,
             **{
                 key: value
                 for key, value in highest_quality_file.items()
-                if key in download_fields
+                if key in video_fields
             },
         )
